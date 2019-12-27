5198f5fd52f4c19e205e34cbb161dc7c1b92476f
Zsuzsanna Erdos <ezsana@gmail.com>
2019-12-27 11:27:39 +0100

    Sales_db_tables changes: orderdate id is pk/fk; product_code varchar (was int); foreign key constrains written properly. Index created for qtr, month, year id.

--- dataset/sales_db_tables.sql
+++ dataset/sales_db_tables.sql
@@ -3,16 +3,16 @@ CREATE TYPE deal_size AS ENUM ('Small', 'Medium', 'Large');
 CREATE TYPE status AS ENUM ('In Process', 'Disputed', 'Shipped', 'Cancelled', 'On Hold', 'Resolved');
 
 CREATE TABLE orderdates (
-    QTR_id quarter_year PRIMARY KEY,
-    month_id int PRIMARY KEY,
-    year_id int PRIMARY KEY,
-    CONSTRAINT orderdates_id PRIMARY KEY (QTR_id, month_id, year_id)
+    orderdates_id date PRIMARY KEY,
+    QTR_id quarter_year NOT NULL ,
+    month_id int NOT NULL,
+    year_id int NOT NULL
 );
 
 CREATE TABLE products (
-    product_code int PRIMARY KEY,
-    product_price float,
-    MSRP float
+    product_code varchar(255) PRIMARY KEY,
+    product_price decimal,
+    MSRP int
 );
 
 CREATE TABLE customer_address (
@@ -41,24 +41,27 @@ CREATE TABLE customers (
     customer_id SERIAL PRIMARY KEY,
     customer_name text,
     phone_number varchar(255),
-    address_id int REFERENCES customer_address, --?????
-    contactname_id int REFERENCES contactnames --????
+    address_id int REFERENCES customer_address(address_id),
+    contactname_id int REFERENCES contactnames(contactname_id)
 );
 
 CREATE TABLE orders (
     order_number int PRIMARY KEY,
-    order_date date,
-    orderdate_id int REFERENCES orderdates, --????
+    orderdates_id date REFERENCES orderdates(orderdates_id),
     status status,
-    customer_id int REFERENCES customers --????
+    customer_id int REFERENCES customers(customer_id)
 );
 
 CREATE TABLE order_items (
-    order_number int REFERENCES orders, --?????
-    product_code int REFERENCES products, --?????
-    quantity int,
-    sales float, -- price * quantity
-    productline_id int REFERENCES productline, --????
+    order_number int REFERENCES orders(order_number),
+    product_code int REFERENCES products(product_code),
+    quantity_ordered int,
+    sales decimal, -- price * quantity
+    productline_id int REFERENCES productline(productline_id),
     dealsize deal_size,
-    orderline int
+    orderline_number int
 );
+
+CREATE INDEX ON orderdates (QTR_id);
+CREATE INDEX ON orderdates (month_id);
+CREATE INDEX ON orderdates (year_id);
