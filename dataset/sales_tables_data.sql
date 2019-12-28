952faf9156d0ba65b8228af5a6d3bc6c4e0e692c
Zsuzsanna Erdos <ezsana@gmail.com>
2019-12-28 10:46:34 +0100

    Orderdates table data inserted.

---
+++ dataset/sales_tables_data.sql
@@ -0,0 +1,10 @@
+-- Data inserted to orderdates
+
+INSERT INTO orderdates
+SELECT TO_DATE(rtrim(substring(order_date, 1, length(order_date)-4)), 'MM/DD/YYYY'),
+       CAST(qtr_id AS quarter_year), CAST(month_id AS integer),
+       CAST(year_id AS integer) FROM temp_all_sales ON CONFLICT DO NOTHING;
+
+-- Data inserted to
+
+
